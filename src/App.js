980dd9eafeab98f70b0e84b7f267c87248bc3ee4
Erik Olsson <erik@erikolson.dev>
2020-05-24 14:21:18 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import TextEditor from "./components/TextEditor"
-import Tiny from "./components/Tiny"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <TextEditor></TextEditor>
-      {/* <Tiny></Tiny> */}
     </div>
   );
 }
